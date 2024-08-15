bd740865f17fa50b84d53796a653d836a18b8fe2
Robin Malfait <malfait.robin@gmail.com>
2024-08-15 12:04:59 +0200

    add `NOT()`

--- app/domain/functions/logic.ts
+++ app/domain/functions/logic.ts
@@ -23,3 +23,21 @@ export function AND(...args: EvaluationResult[]): EvaluationResult {
 export function OR(...args: EvaluationResult[]): EvaluationResult {
   return args.some((arg) => arg.value) ? TRUE() : FALSE()
 }
+
+export function NOT(lhs: EvaluationResult, extra?: EvaluationResult): EvaluationResult {
+  if (extra) {
+    throw Object.assign(
+      new Error(`NOT() does not take a second argument, got ${extra.value}`),
+      { short: '#VALUE!' },
+    )
+  }
+
+  if (lhs.kind === EvaluationResultKind.STRING) {
+    throw Object.assign(
+      new Error(`NOT() expects a boolean as the first argument, got ${lhs.value}`),
+      { short: '#VALUE!' },
+    )
+  }
+
+  return lhs.value ? FALSE() : TRUE()
+}
