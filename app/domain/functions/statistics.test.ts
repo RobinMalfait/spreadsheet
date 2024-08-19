e7497b3a3c6dc68908394314c743a16fc1f8be11
Robin Malfait <malfait.robin@gmail.com>
2024-08-20 01:19:28 +0200

    add `MIN(…)` and `MAX(…)` functions

--- app/domain/functions/statistics.test.ts
+++ app/domain/functions/statistics.test.ts
@@ -2,6 +2,90 @@ import { describe, expect, it } from 'vitest'
 import { Spreadsheet } from '~/domain/spreadsheet'
 import { visualizeSpreadsheet } from '~/test/utils'
 
+describe('MIN()', () => {
+  it('should compute the minimum of a set of numbers', () => {
+    let spreadsheet = new Spreadsheet()
+    spreadsheet.set('A1', '=2')
+    spreadsheet.set('B1', '=3')
+    spreadsheet.set('C1', '=4')
+    spreadsheet.set('A2', '=MIN(A1:C1)')
+
+    expect(visualizeSpreadsheet(spreadsheet)).toMatchInlineSnapshot(`
+      "
+      ┌───┬───┬───┬───┐
+      │   │ A │ B │ C │
+      ├───┼───┼───┼───┤
+      │ 1 │ 2 │ 3 │ 4 │
+      ├───┼───┼───┼───┤
+      │ 2 │ 2 │   │   │
+      └───┴───┴───┴───┘
+      "
+    `)
+  })
+
+  it('should ignore non-number types', () => {
+    let spreadsheet = new Spreadsheet()
+    spreadsheet.set('A1', '=2')
+    spreadsheet.set('B1', '=3')
+    spreadsheet.set('C1', '=TRUE()')
+    spreadsheet.set('A2', '=MIN(A1:C1, 4, NOW())')
+
+    expect(visualizeSpreadsheet(spreadsheet)).toMatchInlineSnapshot(`
+      "
+      ┌───┬───┬───┬──────┐
+      │   │ A │ B │ C    │
+      ├───┼───┼───┼──────┤
+      │ 1 │ 2 │ 3 │ TRUE │
+      ├───┼───┼───┼──────┤
+      │ 2 │ 2 │   │      │
+      └───┴───┴───┴──────┘
+      "
+    `)
+  })
+})
+
+describe('MAX()', () => {
+  it('should compute the minimum of a set of numbers', () => {
+    let spreadsheet = new Spreadsheet()
+    spreadsheet.set('A1', '=2')
+    spreadsheet.set('B1', '=3')
+    spreadsheet.set('C1', '=4')
+    spreadsheet.set('A2', '=MAX(A1:C1)')
+
+    expect(visualizeSpreadsheet(spreadsheet)).toMatchInlineSnapshot(`
+      "
+      ┌───┬───┬───┬───┐
+      │   │ A │ B │ C │
+      ├───┼───┼───┼───┤
+      │ 1 │ 2 │ 3 │ 4 │
+      ├───┼───┼───┼───┤
+      │ 2 │ 4 │   │   │
+      └───┴───┴───┴───┘
+      "
+    `)
+  })
+
+  it('should ignore non-number types', () => {
+    let spreadsheet = new Spreadsheet()
+    spreadsheet.set('A1', '=2')
+    spreadsheet.set('B1', '=3')
+    spreadsheet.set('C1', '=TRUE()')
+    spreadsheet.set('A2', '=MAX(A1:C1, 4, NOW())')
+
+    expect(visualizeSpreadsheet(spreadsheet)).toMatchInlineSnapshot(`
+      "
+      ┌───┬───┬───┬──────┐
+      │   │ A │ B │ C    │
+      ├───┼───┼───┼──────┤
+      │ 1 │ 2 │ 3 │ TRUE │
+      ├───┼───┼───┼──────┤
+      │ 2 │ 4 │   │      │
+      └───┴───┴───┴──────┘
+      "
+    `)
+  })
+})
+
 describe('AVERAGE()', () => {
   it('should calculate the average of a set of numbers', () => {
     let spreadsheet = new Spreadsheet()
--- app/domain/functions/statistics.ts
+++ app/domain/functions/statistics.ts
@@ -1,5 +1,47 @@
 import { type EvaluationResult, EvaluationResultKind } from '~/domain/evaluation'
 
+export function MIN(...args: EvaluationResult[]): EvaluationResult {
+  let min = Number.POSITIVE_INFINITY
+
+  for (let arg of args) {
+    switch (arg.kind) {
+      case EvaluationResultKind.NUMBER:
+        min = Math.min(min, arg.value)
+        break
+      case EvaluationResultKind.BOOLEAN:
+      case EvaluationResultKind.STRING:
+      case EvaluationResultKind.DATETIME:
+        // Explicitly ignored
+        break
+      default:
+        arg satisfies never
+    }
+  }
+
+  return { kind: EvaluationResultKind.NUMBER, value: min }
+}
+
+export function MAX(...args: EvaluationResult[]): EvaluationResult {
+  let max = Number.NEGATIVE_INFINITY
+
+  for (let arg of args) {
+    switch (arg.kind) {
+      case EvaluationResultKind.NUMBER:
+        max = Math.max(max, arg.value)
+        break
+      case EvaluationResultKind.BOOLEAN:
+      case EvaluationResultKind.STRING:
+      case EvaluationResultKind.DATETIME:
+        // Explicitly ignored
+        break
+      default:
+        arg satisfies never
+    }
+  }
+
+  return { kind: EvaluationResultKind.NUMBER, value: max }
+}
+
 export function AVERAGE(...args: EvaluationResult[]): EvaluationResult {
   let sum = 0
   let count = 0
